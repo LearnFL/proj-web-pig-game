4987d6a71c2910730e761cf44876218f7a6d48fe
DR <86169204+LearnFL@users.noreply.github.com>
2022-10-29 17:06:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,65 +9,72 @@ const overLay = document.querySelector('.overlay');
 
 let game = false;
 
-window.onload = () => (game = true);
-
 function findActivePlayer() {
   return document.querySelector('.player--active');
 }
 
 rollBotton.addEventListener('click', () => {
-  if (game) {
-    diceImage.classList.remove('hidden');
+  if (game === false) game = true;
+  diceImage.classList.remove('hidden');
 
-    //   Roll a dice
-    const roll = Math.trunc(Math.random() * 6) + 1;
+  //   Roll a dice
+  const roll = Math.trunc(Math.random() * 6) + 1;
 
-    //   If got 1 then loose scores and move to next player
-    if (Number(roll) === 1) {
-      diceImage.setAttribute('src', `dice-${roll}.png`);
-      players.forEach(player => {
-        if (player.classList.contains('player--active')) {
-          player.classList.remove('player--active');
-          player.querySelector('.current-score').textContent = 0;
-        } else {
-          player.classList.add('player--active');
-        }
-      });
-    } else {
-      diceImage.setAttribute('src', `dice-${roll}.png`);
-      let activePlayer = findActivePlayer();
-      let currentScore =
-        activePlayer.querySelector('.current-score').textContent;
-      activePlayer.querySelector('.current-score').textContent =
-        Number(currentScore) + Number(roll);
-    }
+  // Show dice
+  diceImage.setAttribute('src', `dice-${roll}.png`);
+
+  //   If got 1 then loose scores and move to next player
+  if (Number(roll) === 1) {
+    players.forEach(player => {
+      if (player.classList.contains('player--active')) {
+        player.classList.remove('player--active');
+        player.querySelector('.current-score').textContent = 0;
+      } else {
+        player.classList.add('player--active');
+      }
+    });
+  } else {
+    let activePlayer = findActivePlayer();
+    let currentScore = activePlayer.querySelector('.current-score').textContent;
+    activePlayer.querySelector('.current-score').textContent =
+      Number(currentScore) + Number(roll);
   }
 });
 
 holdBotton.addEventListener('click', () => {
+  // Hold allowe3d only if game started
   if (game) {
     let activePlayer = findActivePlayer();
     let score = Number(activePlayer.querySelector('.score').textContent);
-    activePlayer.querySelector('.score').textContent =
-      score + Number(activePlayer.querySelector('.current-score').textContent);
-    activePlayer.querySelector('.current-score').textContent = 0;
-    if (Number(activePlayer.querySelector('.score').textContent) >= 100) {
-      diceImage.classList.add('hidden');
-      activePlayer.style.backgroundColor = '#3c4048a4';
-      game = false;
-    } else {
-      players.forEach(player => {
-        if (player.classList.contains('player--active')) {
-          player.classList.remove('player--active');
-        } else {
-          player.classList.add('player--active');
-        }
-      });
+
+    // Hold is allowed only if player rolled dice
+    if (
+      Number(activePlayer.querySelector('.current-score').textContent) !== 0
+    ) {
+      activePlayer.querySelector('.score').textContent =
+        score +
+        Number(activePlayer.querySelector('.current-score').textContent);
+      activePlayer.querySelector('.current-score').textContent = 0;
+
+      // Check if player is the winner
+      if (Number(activePlayer.querySelector('.score').textContent) >= 100) {
+        diceImage.classList.add('hidden');
+        activePlayer.style.backgroundColor = '#3c4048a4';
+        game = false;
+      } else {
+        players.forEach(player => {
+          if (player.classList.contains('player--active')) {
+            player.classList.remove('player--active');
+          } else {
+            player.classList.add('player--active');
+          }
+        });
+      }
     }
   }
 });
 
+// New game
 newGameBotton.addEventListener('click', () => {
   location.reload();
-  game = true;
 });
